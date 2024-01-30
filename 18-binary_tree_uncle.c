fe24302948c109f6ed90ada071395ce054386c20
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-30 15:53:14 +0000

    Found the tree's uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,12 +10,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
-	binary_tree_t *grand_parent = node->parent->parent;
+	binary_tree_t *grand_parent;
 
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
+	grand_parent = node->parent->parent;
 
 	if (grand_parent->left == node->parent)
 		return (grand_parent->right ? grand_parent->right : NULL);
