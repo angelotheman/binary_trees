14010cdbecb95c7cd2aec44089700f2b5daba791
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-30 15:50:25 +0000

    Fixed betty error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
