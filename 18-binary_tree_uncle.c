71066aeeb343a28c9e106c6bc56f5842d6a05d44
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-30 14:52:16 +0000

    Find the uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ *
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the node uncle, otherwise NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grand_parent = node->parent->parent;
+	
+	if (!node || !node->parent || !grand_parent)
+		return (NULL);
+
+	if (grand_parent->left == node->parent)
+		return (grand_parent->right ? grand_parent->right : NULL);
+	else
+		return (grand_parent->left ? grand_parent->left : NULL);
+}
