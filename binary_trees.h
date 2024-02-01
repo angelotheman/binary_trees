181bd010c36dc71ef0d6b3e791b9715dcfa46d42
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-01 21:35:15 +0000

    Updated with main advanced header files

--- binary_trees.h
+++ binary_trees.h
@@ -64,12 +64,23 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 /* Task Functions (Helper Functions) */
 void traverse_level(const binary_tree_t *node, int level, void(*func)(int));
 int is_complete(const binary_tree_t *tree, int index, int size);
 int is_bst_util(const binary_tree_t *tree, const binary_tree_t **prev);
-bst_t *bst_find_successor(bst_t *node);
 void binary_delete(binary_tree_t *tree);
+bst_t *bst_find_successor(bst_t *node);
+
 
 #endif /* BINARY_TREES_H */
